68c47bb8cfdafb9c910f97aab64c7f0b5fb52de0
Malinicz <malinicz@gmail.com>
2019-03-17 17:02:23 +0100

    fix config

--- src/services/contentfulClient/index.js
+++ src/services/contentfulClient/index.js
@@ -1,8 +1,8 @@
 const contentful = require('contentful');
 
 const client = contentful.createClient({
-  space: process.env.CONTENTFUL_SPACE_ID || '',
-  accessToken: process.env.CONTENTFUL_TOKEN || '',
+  space: (process && process.env && process.env.CONTENTFUL_SPACE_ID) || '',
+  accessToken: (process && process.env && process.env.CONTENTFUL_TOKEN) || '',
   host: 'cdn.contentful.com',
 });
 
