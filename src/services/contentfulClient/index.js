7accd054de678a32d1de09286050def73d521444
Malinicz <malinicz@gmail.com>
2019-03-21 00:00:40 +0100

    add netlify _redirects config

--- src/services/contentfulClient/index.js
+++ src/services/contentfulClient/index.js
@@ -1,12 +1,8 @@
 const contentful = require('contentful');
 
 const client = contentful.createClient({
-  space:
-    (process && process.env && process.env.CONTENTFUL_SPACE_ID) ||
-    '35ue3xk6d2pg',
-  accessToken:
-    (process && process.env && process.env.CONTENTFUL_TOKEN) ||
-    'd0ef590c8ee88bdd4b3e256ef234e91062cee9f67c3edbe9a1ddafc10bc9b415',
+  space: (process && process.env && process.env.CONTENTFUL_SPACE_ID) || '',
+  accessToken: (process && process.env && process.env.CONTENTFUL_TOKEN) || '',
   host: 'cdn.contentful.com',
 });
 
